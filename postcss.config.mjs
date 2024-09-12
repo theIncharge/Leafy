ac643a89be66172e96f643fac27ddf12bc1e8311
theIncharge <adityagaur1741@gmail.com>
2024-09-12 22:59:19 +0530

    add half landing page

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
 };
 
 export default config;
